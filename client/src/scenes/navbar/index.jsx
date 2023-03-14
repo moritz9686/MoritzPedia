9ce54e2d9109f111226d24db7f9f10889b8cdf95
moritz9686 <109568693+moritz9686@users.noreply.github.com>
2023-03-14 12:03:04 +0530

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Moritzpedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
